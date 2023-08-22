97b6ea9146fba2d02e0959b17343b62fe89a4c04
Shehroz <mshehroz302@gmail.com>
2023-08-22 21:42:01 +0500

    Setting code
    check tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="text-3xl bg-slate-600">Hello</div>
+      <div>
+        <h1 className="text-orange-800 text-center text-6xl">Hello Pakistan</h1>
+      </div>
     </>
   );
 }
